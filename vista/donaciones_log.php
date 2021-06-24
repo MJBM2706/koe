dd22269e47dff5f5a549b70864b8335d0a23ac7f
MJBM2706 <mjboterom68@misena.edu.co>
2021-06-24 12:33:16 -0500

    Majo - trabaja con nosotros Log y donaciones Log

---
+++ vista/donaciones_log.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Koe</title>
+    <link rel="icon" type="image/png" href="../icons/koe.png">
+    <link rel="stylesheet" type="text/css"  href="../css/bootstrap.css">
+    <link rel="stylesheet" type="text/css"  href="../css/estilos.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+      <a class="navbar-brand" href="#"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse  justify-content-center" id="collapsibleNavbar">
+        <ul class="navbar-nav ">
+          <li class="nav-item">
+            <a class="nav-link" href="../index.php"><img src="../../icons/koe.png" alt="Logo" width="30px"></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../conocenos.php">Conócenos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../trabaja_con_nosotros.php">Trabaja con Nosotros</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../donaciones.php">Donaciones</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../../controlador/cerrar.php">Cerrar Sesión</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <div class="contenedor">
+    <div class="container">
+      <div class="col-md row-cols-sm-1 >">
+        <h1>Donaciones</h1>        
+        <h4>¡Ayúdanos!</h4> 
+        <div width="250px" class="wrapper">
+          <form action="https://www.paypal.com/donate" method="post" target="_top">
+                <input type="hidden" name="hosted_button_id" value="QEM5YMT8R746W" />
+                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                <img alt="" border="0" src="https://www.paypal.com/en_CO/i/scr/pixel.gif"  />
+          </form>
+        </div>
+      </div>
+    </div>
+    </div>    
+    <footer>
+    <div class="footer">
+      <nav class="navbar navbar-expand-sm navbar-dark bg-primary justify-content-center">
+        <ul class="navbar-nav ">
+          <li class="nav-item">
+            <a class="nav-link" href="../vista/conocenos.php">Conócenos</a>
+          </li> 
+          <li class="nav-item">
+            <a class="nav-link" href="../vista/trabaja_con_nosotros.php">Trabaja con Nosotros</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../vista/donaciones.php">Donaciones</a>
+          </li> 
+        </ul>
+      </nav>
+      <nav class="navbar navbar-expand-sm navbar-dark bg-primary justify-content-center">
+        <ul class="navbar-nav ">
+          <li class="nav-item">
+            <a class="nav-link" href="#"><img src="../../icons/twitter.png" alt="Twitter" width="30px"></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><img src="../../icons/facebook.png" alt="Facebook" width="30px"></a>
+          </li>
+        </ul>
+      </nav>
+      <nav class="navbar navbar-expand-sm navbar-dark bg-primary justify-content-center">
+        <ul class="navbar-nav ">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Copyright© 2021</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Privacy Policy</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+
+
+  </footer>
+
+</body>
+
+</html>
