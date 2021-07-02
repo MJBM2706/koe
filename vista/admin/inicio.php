c3622abd4307acbaaa6c274ee7f4081caaf64e87
MJBM2706 <mjboterom68@misena.edu.co>
2021-07-02 15:49:44 -0500

    Majo - actualización de rutas

--- vista/admin/inicio.php
+++ vista/admin/inicio.php
@@ -13,8 +13,8 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
-  <header>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-primary" >
+<header>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
       <a class="navbar-brand" href="#"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
         <span class="navbar-toggler-icon"></span>
@@ -22,30 +22,26 @@
       <div class="collapse navbar-collapse  justify-content-center" id="collapsibleNavbar">
         <ul class="navbar-nav ">
           <li class="nav-item">
-              <a class="nav-link" href="../index.php"><img src="../../icons/koe.png" alt="Logo" width="30px"></a>
+            <a class="nav-link" href="../../index.php"><img src="../../icons/koe.png" alt="Logo" width="30px"></a>
           </li>
           <li class="nav-item">
-            <a class="active nav-link" href="#">Inicia Sesión</a>
+            <a class="nav-link" href="../conocenos_log.php">Conócenos</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="registro.vista.php">Registrate</a>
+            <a class="nav-link" href="../trabaja_con_nosotros_log.php">Trabaja con Nosotros</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="conocenos.vista.php">Conócenos</a>
-          </li> 
-          <li class="nav-item">
-            <a class="nav-link" href="trabajaConNosotros.vista.php">Trabaja con Nosotros</a>
+            <a class="nav-link" href="../donaciones_log.php">Donaciones</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="donaciones.vista.php">Donaciones</a>
-          </li>  
+            <a class="nav-link" href="../../controlador/cerrar.php">Cerrar Sesión</a>
+          </li>
         </ul>
-      </div>  
+      </div>
     </nav>
   </header>
  <div class="wrapper fadeInDown">
      <form style="width: 70%;">
-     <!-- HACER VISIBLE ESTE CONTENEDOR, SÍ EL PERFIL ES ADMINISTRADOR -->
     <div id="editarRegistros">
         <div class="container">
             <div class="row justify-content-center P-3">
@@ -143,53 +139,45 @@
  </div>
 
 <!-- PIE -->
-    <footer>
-      <div class="footer">
-        <nav class="navbar navbar-expand-sm navbar-dark bg-primary justify-content-center" >
-        
-        
-          <ul class="navbar-nav ">
-            <li class="nav-item">
-              <a class="active nav-link" href="#">Inicia Sesión</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="../controlador/RegistroController.php">Registrate</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="conocenos.vista.php">Conócenos</a>
-            </li> 
-            <li class="nav-item">
-              <a class="nav-link" href="trabajaConNosotros.vista.php">Trabaja con Nosotros</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="donaciones.vista.php">Donaciones</a>
-            </li>  
-          </ul>
-        </nav>
-        <nav class="navbar navbar-expand-sm navbar-dark bg-primary justify-content-center" >
-          <ul class="navbar-nav ">
-            <li class="nav-item">
-              <a class="nav-link" href="#"><img src="../../icons/twitter.png" alt="Twitter" width="30px"></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#"><img src="../../icons/facebook.png" alt="Facebook" width="30px"></a>
-            </li>
-          </ul>
-        </nav>
-        <nav class="navbar navbar-expand-sm navbar-dark bg-primary justify-content-center" >
-          <ul class="navbar-nav ">
-            <li class="nav-item">
-            <a class="nav-link" href="#">Copyright© 2021</a> 
-            </li>
-            <li class="nav-item">
+<footer>
+    <div class="footer">
+      <nav class="navbar navbar-expand-sm navbar-dark bg-primary justify-content-center">
+        <ul class="navbar-nav ">
+          <li class="nav-item">
+            <a class="nav-link" href="../conocenos_log.php">Conócenos</a>
+          </li> 
+          <li class="nav-item">
+            <a class="nav-link" href="../trabaja_con_nosotros_log.php">Trabaja con Nosotros</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../donaciones_log.php">Donaciones</a>
+          </li> 
+        </ul>
+      </nav>
+      <nav class="navbar navbar-expand-sm navbar-dark bg-primary justify-content-center">
+        <ul class="navbar-nav ">
+          <li class="nav-item">
+            <a class="nav-link" href="#"><img src="../../icons/twitter.png" alt="Twitter" width="30px"></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><img src="../../icons/facebook.png" alt="Facebook" width="30px"></a>
+          </li>
+        </ul>
+      </nav>
+      <nav class="navbar navbar-expand-sm navbar-dark bg-primary justify-content-center">
+        <ul class="navbar-nav ">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Copyright© 2021</a>
+          </li>
+          <li class="nav-item">
             <a class="nav-link" href="#">Privacy Policy</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-   
+          </li>
+        </ul>
+      </nav>
+    </div>
 
-    </footer>
+
+  </footer>
 
 </body>
 
