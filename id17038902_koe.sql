6f8f30cc610e703ea1d179759089609aa9cb3f73
MJBM2706 <mjboterom68@misena.edu.co>
2021-07-12 18:59:22 -0500

    Actualización - base de datos

--- id17038902_koe.sql
+++ id17038902_koe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-07-2021 a las 21:07:43
+-- Tiempo de generación: 13-07-2021 a las 01:58:44
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.10
 
@@ -47,9 +47,8 @@ CREATE TABLE `antecedentes_historia` (
 
 CREATE TABLE `historia_clinica` (
   `idHistoria_Clinica` bigint(20) NOT NULL,
-  `paciente_idPaciente` bigint(20) NOT NULL,
   `paciente_Usuario_idUsuario` bigint(20) NOT NULL,
-  `profesional_idProfesional` bigint(20) NOT NULL,
+  `profesional_Usuario_idUsuario` bigint(20) NOT NULL,
   `fecha` date NOT NULL DEFAULT current_timestamp(),
   `fuenteInformacion` text COLLATE utf8_unicode_ci NOT NULL,
   `motivoConsulta` text COLLATE utf8_unicode_ci NOT NULL,
@@ -88,8 +87,8 @@ CREATE TABLE `paciente` (
 
 INSERT INTO `paciente` (`usuario_idUsuario`, `nombre`, `apellido`, `tipoDocumento`, `documentoIdentidad`, `fechaNacimiento`, `departamento`, `ciudad`, `direccion`, `celular`, `contactoEmergencia`, `celularEmergencia`) VALUES
 (20, 'Maria José', 'Botero Martínez', 'Cédula', '1036963286', '1998-06-27', 'Antioquia', 'Medellín', 'Cra 54C #66b-37', '3002427778', 'Luciana Martinez', '3002427778'),
-(22, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(27, 'Lorenzo', 'Ryuuji', 'Cédula', '1036963286', '2001-03-19', NULL, NULL, NULL, NULL, NULL, NULL);
+(27, 'Lorenzo', 'Ryuuji', 'Cédula', '1036963286', '2001-03-19', NULL, NULL, NULL, NULL, NULL, NULL),
+(31, 'Luciana', 'Martínez Riaza', 'Cédula', '43834665', '1976-09-01', NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -106,13 +105,21 @@ CREATE TABLE `profesional` (
   `fechaNacimiento` date NOT NULL,
   `departamento` varchar(125) COLLATE utf8_unicode_ci NOT NULL,
   `ciudad` varchar(125) COLLATE utf8_unicode_ci NOT NULL,
+  `direccion` varchar(300) COLLATE utf8_unicode_ci NOT NULL,
+  `celular` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `tituloProfesional` varchar(125) COLLATE utf8_unicode_ci NOT NULL,
   `tarjetaProfesional` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `estadoTarjeta` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `estado` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `direccion` varchar(300) COLLATE utf8_unicode_ci NOT NULL,
-  `celular` varchar(45) COLLATE utf8_unicode_ci NOT NULL
+  `estado` int(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `profesional`
+--
+
+INSERT INTO `profesional` (`usuario_idUsuario`, `nombre`, `apellido`, `tipoDocumento`, `documentoIdentidad`, `fechaNacimiento`, `departamento`, `ciudad`, `direccion`, `celular`, `tituloProfesional`, `tarjetaProfesional`, `estadoTarjeta`, `estado`) VALUES
+(30, 'Pepito', 'Londoño', 'Cédula', '1036963286', '2021-07-29', 'Antioquia', 'Medellín', 'Cra 100 #45', '3002427778', 'Psicólogo', '10258546', 'Ok', 1),
+(37, 'Juan', 'López', 'Cédula', '4485324', '2021-07-30', 'Amazonas', 'Leticia', 'Cra 45 #67-30', '3002427778', 'Psiquiatra', '102525546', 'En proceso', 0);
 
 -- --------------------------------------------------------
 
@@ -133,9 +140,13 @@ CREATE TABLE `usuario` (
 
 INSERT INTO `usuario` (`idUsuario`, `tipoUsuario`, `correo`, `password`) VALUES
 (20, 'paciente', 'majo.botero.m.123@gmail.com', '2706'),
-(21, 'admin', 'admin@admin.com', 'admin123456789'),
+(21, 'admin', 'admin@admin.com', '123456789'),
 (22, 'paciente', 'ejemplo@ejemplo', '123456'),
-(27, 'paciente', 'lorenxd1@gmail.com', '12345');
+(27, 'paciente', 'lorenxd1@gmail.com', '12345'),
+(30, 'profesional', 'pepito@profesional.com', '123'),
+(31, 'paciente', 'lucianamart1@hotmail.com', 'dolly2011x'),
+(36, 'voluntario', 'pepito@voluntario.com', '123'),
+(37, 'profesional', 'ejemplo2@profesional.com', '123');
 
 -- --------------------------------------------------------
 
@@ -147,10 +158,24 @@ CREATE TABLE `voluntario` (
   `usuario_idUsuario` bigint(20) NOT NULL,
   `nombre` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `apellido` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `tipoDocumento` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `documento` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `fechaNacimiento` date NOT NULL,
+  `departamento` varchar(125) COLLATE utf8_unicode_ci NOT NULL,
+  `ciudad` varchar(125) COLLATE utf8_unicode_ci NOT NULL,
+  `direccion` varchar(300) COLLATE utf8_unicode_ci NOT NULL,
+  `celular` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `ocupacion` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `estadoCapacitacion` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `estado` varchar(45) COLLATE utf8_unicode_ci NOT NULL
+  `estado` int(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `voluntario`
+--
+
+INSERT INTO `voluntario` (`usuario_idUsuario`, `nombre`, `apellido`, `tipoDocumento`, `documento`, `fechaNacimiento`, `departamento`, `ciudad`, `direccion`, `celular`, `ocupacion`, `estadoCapacitacion`, `estado`) VALUES
+(36, 'Majo', 'Botero', 'Cédula', '1036963286', '2021-07-22', 'Antioquia', 'Medellín', 'Carrera 54 C #66B - 37', '3002427778', 'Estudiante', 'En Proceso', 1);
 
 --
 -- Índices para tablas volcadas
@@ -172,12 +197,14 @@ ALTER TABLE `historia_clinica`
 -- Indices de la tabla `paciente`
 --
 ALTER TABLE `paciente`
+  ADD PRIMARY KEY (`usuario_idUsuario`),
   ADD KEY `fk_Paciente_Usuario_idUsuario` (`usuario_idUsuario`);
 
 --
 -- Indices de la tabla `profesional`
 --
 ALTER TABLE `profesional`
+  ADD PRIMARY KEY (`usuario_idUsuario`),
   ADD KEY `fk_Profesional_Usuario_idUsuario` (`usuario_idUsuario`);
 
 --
@@ -191,17 +218,25 @@ ALTER TABLE `usuario`
 -- Indices de la tabla `voluntario`
 --
 ALTER TABLE `voluntario`
-  ADD KEY `fk_Voluntario_Usuario_idUsuario` (`usuario_idUsuario`);
+  ADD PRIMARY KEY (`usuario_idUsuario`),
+  ADD KEY `fk_Voluntario_Usuario_idUsuario` (`usuario_idUsuario`),
+  ADD KEY `usuario_idUsuario` (`usuario_idUsuario`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `historia_clinica`
+--
+ALTER TABLE `historia_clinica`
+  MODIFY `idHistoria_Clinica` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idUsuario` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `idUsuario` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- Restricciones para tablas volcadas
@@ -212,12 +247,6 @@ ALTER TABLE `usuario`
 --
 ALTER TABLE `antecedentes_historia`
   ADD CONSTRAINT `antecedentes_historia_ibfk_1` FOREIGN KEY (`paciente_Usuario_idUsuario`) REFERENCES `usuario` (`idUsuario`) ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `historia_clinica`
---
-ALTER TABLE `historia_clinica`
-  ADD CONSTRAINT `historia_clinica_ibfk_1` FOREIGN KEY (`idHistoria_Clinica`) REFERENCES `usuario` (`idUsuario`) ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `paciente`
